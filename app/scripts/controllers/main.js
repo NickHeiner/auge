4b20dcb03772cb591dfa54123278a11d47964bf1
Nick Heiner <nick.heiner@opower.com>
2013-10-06 12:26:10 -0400

    initial import from yo-angular

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('augeApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
