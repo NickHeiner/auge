958800485ae826f47eb583c0287e1023f19579f0
Nick Heiner <nick.heiner@opower.com>
2013-10-06 13:37:47 -0400

    works, not terribly

--- app/scripts/controllers/write.js
+++ app/scripts/controllers/write.js
@@ -4,18 +4,13 @@ angular.module('augeApp')
     .controller('WriteCtrl', function($scope, startFirebase, $routeParams) {
         startFirebase($routeParams.sessionId, $scope, 'sensor');
 
-        $scope.update = {
-            count: 0
-        };
-
         window.addEventListener('deviceorientation', function(eventData) {
             $scope.$apply(function() {
+
+                // for some reason _.merge or _.assign won't work here, and I don't know why.
                 ['alpha', 'beta', 'gamma'].forEach(function(prop) {
                    $scope.sensor[prop] = eventData[prop];
                 });
-//                $scope.sensor.alpha = eventData.alpha;
-//                _.assign($scope.sensor, eventData);
-                $scope.update.count++;
             });
         });
 
